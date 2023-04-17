26d6fcc5b4db553d1db88a02c783548e4f32412a
danniellabot <danniellanas@gmail.com>
2023-04-17 22:15:47 +0100

    add

--- src/texas/general.js
+++ src/texas/general.js
@@ -74,3 +74,32 @@ const instanceMock = {
     expect(instanceMock.deleteNode).toHaveBeenCalledWith('node1');
   });
   
+  const instanceMock = {
+    getNodes: jest.fn().mockReturnValue([{ id: 'node1', data: { nodeType: 'Question' } }, { id: 'node2', data: { nodeType: 'Answer' } }]),
+    getEdges: jest.fn().mockReturnValue([]),
+    deleteNode: jest.fn(),
+  };
+  
+  test('should call deleteNode when a source node is connected to a target node', async () => {
+    const onConnectMock = jest.fn();
+    const { getByLabelText } = render(
+      <>
+        <Handle type="source" position="bottom" node={{ id: 'node1', data: { nodeType: 'Question' } }} aria-label="source-node1" />
+        <Handle type="target" position="top" node={{ id: 'node2', data: { nodeType: 'Answer' } }} onConnect={onConnectMock} aria-label="target-node2">
+          <div data-testid="figure" />
+        </Handle>
+      </>
+    );
+  
+    const sourceHandle = getByLabelText('source-node1');
+    const targetHandle = getByLabelText('target-node2');
+  
+    fireEvent.mouseDown(sourceHandle);
+    fireEvent.mouseDown(targetHandle);
+    fireEvent.mouseUp(targetHandle);
+  
+    await waitFor(() => expect(onConnectMock).toHaveBeenCalled());
+  
+    expect(instanceMock.deleteNode).toHaveBeenCalledWith('node1');
+  });
+  
